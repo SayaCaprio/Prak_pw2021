c3d189534e6156ba0e378859636ffbbb5e9cd508
sayacaprio <caprio.sinaga16@gmail.com>
2021-03-05 02:10:06 +0700

    menyelesaikan prak pw

--- tugas1.php
+++ tugas1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tugas1 Ibnu</title>
+    <title>tugas1 caprio</title>
     <style>
         .kotak{
             width: 70px;
